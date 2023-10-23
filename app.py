bfed27fbb7ac7517a2b0bc27d25c9bb42e87cde8
palwithpen <69918906+palwithpen@users.noreply.github.com>
2023-10-23 08:24:45 +0000

    Added Externalizaton of routes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Body
 import uvicorn
+from routes.user_routes import router as user_router
+
 app = FastAPI()
 
 """
@@ -8,7 +10,7 @@ READ - get
 UPDATE - put
 DELETE - delete
 """
-
+"""
 @app.get("/")
 def check_connectivity():
     return {"status_code":200, "status":"connected"}
@@ -16,7 +18,9 @@ def check_connectivity():
 @app.post("/save/data")
 def save_user_data(user_data: dict = Body(...)):
     return {"status_code":201, "status":"success", "data":user_data}
+"""
 
+app.include_router(user_router)
 
 if __name__ == "__main__" :
     uvicorn.run(app, host="0.0.0.0", port=8080)
